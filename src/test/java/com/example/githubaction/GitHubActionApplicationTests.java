3a670bcd16fa3f310222569e3f30b3df55547d06
HkHino <mpfugl@hotmail.com>
2023-02-08 10:24:17 +0100

    stuf

---
+++ src/test/java/com/example/githubaction/GitHubActionApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.githubaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubActionApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
